7e0c6940e2608fad02a73e1bf1b1031fda156375
aBerglehner <alexanderberglehner2@gmail.com>
2023-06-29 23:58:21 +0200

    2018 day2 part2

--- 2018/day2/src/Second.java
+++ 2018/day2/src/Second.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 
 public class Day1 {
     public static void main(String[] args) throws Exception {
@@ -12,39 +10,53 @@ public class Day1 {
         BufferedReader reader = new BufferedReader(
             new FileReader(file)
         );
-        System.out.println("---------------------");
+        // System.out.println("---------------------");
         // TODO: second test todo
-        List<Integer> allResults = new ArrayList<Integer>();
+        List<List<String>> allResults = new ArrayList<>();
 
         String line;
-        Integer twoCount = 0;
-        Integer threeCount = 0;
         while ((line = reader.readLine()) != null) {
             // Process each line of the file
             // System.out.println(line);
-            Map<String, Integer> myMap = new HashMap<>();
+            List<String> curList = new ArrayList<>();
             for(String c : line.split("")) {
                 // System.out.println(c);
-                if(myMap.containsKey(c)) {
-                    Integer last = myMap.get(c);
-                    myMap.put(c, last + 1);
-                } else {
-                    myMap.put(c,1);
+                curList.add(c);
+            }
+            allResults.add(curList);
+        }
+        // System.out.println("---------------------");
+        // System.out.println("allResults: " + allResults);
+        // System.out.println("---------------------");
+
+        int rows = allResults.size();
+        int cols = allResults.get(0).size();
+        for(int row = 0; row < rows - 1; ++row) {
+            List<String> curRow = allResults.get(row);
+            for(int j = row + 1; j < rows; ++j) {
+                int diffs = 0;
+                List<String> nextRow = allResults.get(j);
+                // System.out.println("curRow-" + row + ": " + curRow);
+                for(int col = 0; col < cols; ++col) {
+                    String cur = curRow.get(col);
+                    String next = nextRow.get(col);
+                    // System.out.println("cur: " + cur);
+                    // System.out.println("next: " + next);
+                    // System.out.println("---------------------");
+                    if(!cur.equals(next)) {
+                        diffs++;
+                    }
+                }
+                // System.out.println("###################################");
+                // System.out.println("diffs: " + diffs);
+                if(diffs == 1) {
+                    System.out.println(String.join("",curRow));
+                    // System.out.println("result_row-" + row + ": " + String.join("",curRow));
+                    // System.out.println("next___row-" + j + ": " + nextRow);
                 }
             }
-            // System.out.println(myMap);
-            // System.out.println("$$$$$$$$$$$");
-            //
-            // System.out.println(myMap.containsValue(2) || myMap.containsValue(3));
-            // System.out.println("---------------------");
-            if(myMap.containsValue(2)) twoCount += 1; 
-            if(myMap.containsValue(3)) threeCount += 1;
-
         }
-        System.out.println("---------------------");
-        System.out.println("twoCount: " + twoCount);
-        System.out.println("threeCount: " + threeCount);
-        System.out.println("result: " + twoCount * threeCount);
+
 
     }
 }
