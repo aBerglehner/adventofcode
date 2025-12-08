888b4b58f3d93ccc14dda67a16d7bd33fd6a2ae1
aBerglehner <alexanderberglehner2@gmail.com>
2025-12-09 00:12:53 +0100

    feat: add utils

--- go/utils/utils.go
+++ go/utils/utils.go
@@ -50,3 +50,17 @@ func GetDis() []Pos {
 func GetDirs() []Pos {
 	return []Pos{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
 }
+
+func SplitTasks[T any](tasks []T, n int) [][]T {
+	var chunks [][]T
+	length := len(tasks)
+	if n <= 0 {
+		n = 1
+	}
+	for i := 0; i < n; i++ {
+		start := i * length / n
+		end := (i + 1) * length / n
+		chunks = append(chunks, tasks[start:end])
+	}
+	return chunks
+}
